ed1dc8e3d53f08fc987c9ed33b2c4b43abc1806c
jgigliotti38 <jgigliotti@MJ0994ZS.Sewickley.bnk>
2022-01-11 10:01:06 -0500

    Removed powershell files

--- GET-SSB/Get-SMBOptions.ps1
+++
@@ -1,5 +0,0 @@
-Get-WindowsOptionalFeature -Online -Featurename SMB1Protocol
-Pause
-Get-SmbServerConfiguration | Select-Object EnableSMB2Protocol
-Pause
-Get-SMBConnection | Select-Object ServerName, Signed, Dialect
--- GET-SSB/NetBios-Disable.ps1
+++
@@ -1,3 +0,0 @@
-$key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
-Get-ChildItem $key |
-ForEach-Object { Set-ItemProperty -Path "$key\$($_.pschildname)" -Name NetbiosOptions -Value 2 -Verbose}
